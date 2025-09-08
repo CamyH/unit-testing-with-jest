74ba9af300cb3f809d50d35b9d2b8b5fed347a68
CamyH <cameronhunt88@hotmail.com>
2025-09-08 14:36:31 +0100

    Add tests

--- src/c/component.spec.ts
+++ src/c/component.spec.ts
@@ -7,7 +7,24 @@ describe('SimpleBank', () => {
         component = new SimpleBank();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it('should throw an Error if the deposit amount is < 0', () => {
+        expect(() => component.deposit(-1)).toThrowError();
+    });
+
+    it('should throw an Error if the withdraw amount is < 0', () => {
+        expect(() => component.withdraw(-1)).toThrowError();
+    });
+
+    it('should not throw an Error if the deposit amount is > 0', () => {
+        expect(() => component.deposit(2)).not.toThrowError();
+    });
+
+    it('should not throw an Error if the withdraw amount is > 0', () => {
+        expect(() => component.withdraw(4)).not.toThrowError();
+    });
+
+    it('should return the correct balance', () => {
+        component.deposit(4);
+        expect(component.balance).toEqual(4);
     });
 });
